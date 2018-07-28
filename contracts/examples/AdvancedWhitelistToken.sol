be0383f103064de0cf92af39153e6364830882df
Ron Gierlach <rongierlach@gmail.com>
2018-07-28 08:01:29 -0700

    simplify BasicWhitelistToken.sol

---
+++ contracts/examples/AdvancedWhitelistToken.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.24;
+import "../SimpleRestrictedToken.sol";
+import "zeppelin-solidity/contracts/access/Whitelist.sol";
+
+contract AdvancedWhitelistToken is SimpleRestrictedToken {
+  address public senderWhitelist;
+  address public receiverWhitelist;
+
+  constructor () {
+    senderWhitelist = Whitelist.deploy();
+    receiverWhitelist = Whitelist.deploy();
+  }
+}
+
+// contract BasicWhitelistToken is SimpleRestrictedToken, Whitelist {
+//   function detectTransferRestriction (address from, address to, uint value)
+//     public
+//     constant
+//     returns (uint restrictionCode)
+//   {
+//     if (whitelist(to)) {
+//       restrictionCode = 1; // illegal transfer outside of whitelist
+//     } else {
+//       restrictionCode = 0; // successful transfer (required)
+//     }
+//   }
+
+//   function messageForTransferRestriction (uint restrictionCode)
+//     public
+//     view
+//     return (string message)
+//   {
+//     if (restrictionCode == 0) {
+//       message = "SUCCESS"; // required
+//     } else if (restrictionCode == 1) {
+//       message = "ILLEGAL_TRANSFER_TO_NON_WHITELISTED_ADDRESS";
+//     } else {
+//       message = "UNKNOWN"; // fallback
+//     }
+//   }
+// }
--- contracts/examples/BasicWhitelistToken.sol
+++ contracts/examples/BasicWhitelistToken.sol
@@ -1,31 +1,21 @@
 pragma solidity ^0.4.24;
-import "../SimpleRestrictedToken.sol";
+import "../MessagedSRS20.sol";
 import "zeppelin-solidity/contracts/access/Whitelist.sol";
 
-contract BasicWhitelistToken is SimpleRestrictedToken, Whitelist {
-  function detectTransferRestriction (address from, address to, uint value)
-    public
-    constant
-    returns (uint restrictionCode)
-  {
-    if (whitelist(to)) {
-      restrictionCode = 1; // illegal transfer outside of whitelist
-    } else {
-      restrictionCode = 0; // successful transfer (required)
+contract BasicWhitelistToken is MessagedSRS20, Whitelist {
+    constructor () {
+        messagesAndCodes.addMessage(1, "ILLEGAL_TRANSFER_TO_NON_WHITELISTED_ADDRESS");
     }
-  }
 
-  function messageForTransferRestriction (uint restrictionCode)
-    public
-    view
-    return (string message)
-  {
-    if (restrictionCode == 0) {
-      message = "SUCCESS"; // required
-    } else if (restrictionCode == 1) {
-      message = "ILLEGAL_TRANSFER_TO_NON_WHITELISTED_ADDRESS";
-    } else {
-      message = "UNKNOWN"; // fallback
+    function detectTransferRestriction (address from, address to, uint value)
+        public
+        view
+        returns (uint restrictionCode)
+    {
+        if (whitelist(to)) {
+            restrictionCode = 1; // illegal transfer outside of whitelist
+        } else {
+            restrictionCode = 0; // successful transfer (required)
+        }
     }
-  }
 }
