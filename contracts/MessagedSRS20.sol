17f8086b0eb0f8a64c66ae01ccf2c7d6bc396bd6
Ron Gierlach <rongierlach@gmail.com>
2018-07-28 07:54:19 -0700

    ManagedSRS20.sol reference

---
+++ contracts/MessagedSRS20.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+import "./SimpleRestrictedToken.sol";
+import "./libraries/MessagesAndCodes.sol";
+
+contract MessagedSRS20 is SimpleRestrictedToken {
+    using MessagesAndCodes for MessagesAndCodes.Data;
+    MessagesAndCodes.Data internal messagesAndCodes;
+
+    constructor () {
+        messagesAndCodes.addMessage(0, "SUCCESS");
+    }
+
+    function messageForTransferRestriction (uint restrictionCode)
+        public
+        view
+        returns (string message)
+    {
+        message = messagesAndCodes.messages[restrictionCode];
+    }
+}
