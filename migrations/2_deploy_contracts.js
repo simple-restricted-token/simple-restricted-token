4c8153ec04e18bec69bb47586777b9ebe3c7369b
Ron Gierlach <rongierlach@gmail.com>
2018-07-30 16:52:14 -0700

    link MessagesAndCodes to BasicWhitelistToken

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var SRS20Mock = artifacts.require('./mocks/SRS20Mock')
 var MessagedSRS20Mock = artifacts.require('./mocks/MessagedSRS20Mock')
+var BasicWhitelistTokenMock = artifacts.require(
+  './mocks/BasicWhitelistTokenMock'
+)
 var MessagesAndCodes = artifacts.require('./libraries/MessagesAndCodes')
 
 const initialBalance = '100000000000000000000' // 100 tokens when decimals is 18
@@ -9,8 +12,16 @@ module.exports = function(deployer, network, [initialAccount, ...accounts]) {
     try {
       await deployer.deploy(SRS20Mock, initialAccount, initialBalance)
       await deployer.deploy(MessagesAndCodes)
-      await deployer.link(MessagesAndCodes, [MessagedSRS20Mock])
+      await deployer.link(MessagesAndCodes, [
+        MessagedSRS20Mock,
+        BasicWhitelistTokenMock
+      ])
       await deployer.deploy(MessagedSRS20Mock, initialAccount, initialBalance)
+      await deployer.deploy(
+        BasicWhitelistTokenMock,
+        initialAccount,
+        initialBalance
+      )
     } catch (err) {
       console.log(('Failed to Deploy Contracts', err))
     }
