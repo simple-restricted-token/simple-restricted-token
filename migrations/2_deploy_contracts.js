9b6de8b7a3370026b0e423c03515971b041f4701
Ron Gierlach <rongierlach@gmail.com>
2018-07-30 13:59:07 -0700

    link MessagesAndCodes lib

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var SRS20Mock = artifacts.require('./mocks/SRS20Mock')
-// var MessagesAndCodes = artifacts.require('./libraries/MessagesAndCodes')
+var MessagedSRS20Mock = artifacts.require('./mocks/MessagedSRS20Mock')
+var MessagesAndCodes = artifacts.require('./libraries/MessagesAndCodes')
 
 const initialBalance = '100000000000000000000' // 100 tokens when decimals is 18
 
@@ -7,6 +8,9 @@ module.exports = function(deployer, network, [initialAccount, ...accounts]) {
   deployer.then(async () => {
     try {
       await deployer.deploy(SRS20Mock, initialAccount, initialBalance)
+      await deployer.deploy(MessagesAndCodes)
+      await deployer.link(MessagesAndCodes, [MessagedSRS20Mock])
+      await deployer.deploy(MessagedSRS20Mock, initialAccount, initialBalance)
     } catch (err) {
       console.log(('Failed to Deploy Contracts', err))
     }
