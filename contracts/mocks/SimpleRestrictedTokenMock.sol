929a4bf23a0bb1516c72af31100b6c575512a6b9
Ron Gierlach <rongierlach@gmail.com>
2018-08-14 18:09:52 -0700

    indentation

--- contracts/mocks/SimpleRestrictedTokenMock.sol
+++ contracts/mocks/SimpleRestrictedTokenMock.sol
@@ -4,8 +4,8 @@ import "../token/SRS20/SimpleRestrictedToken.sol";
 
 contract SimpleRestrictedTokenMock is BasicTokenMock, SimpleRestrictedToken {
     constructor (address initialAccount, uint256 initialBalance)
-      BasicTokenMock(initialAccount, initialBalance)
-      public
+        BasicTokenMock(initialAccount, initialBalance)
+        public
     {
 
     }
