70018e6172632e1333225afd618af0b7acb8973f
Ron Gierlach <rongierlach@gmail.com>
2018-08-14 18:10:54 -0700

    mock token for testing

---
+++ contracts/mocks/MaxOwnershipStakeTokenMock.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+import "./BasicTokenMock.sol";
+import "../examples/cap-tables/MaxOwnershipStakeToken.sol";
+
+contract MaxOwnershipStakeTokenMock is MaxOwnershipStakeToken, BasicTokenMock {
+    constructor (address initialAccount, uint256 initialBalance, uint256 _maximumPercentOwnershipTimesOneThousand)
+        BasicTokenMock(initialAccount, initialBalance)
+        MaxOwnershipStakeToken(_maximumPercentOwnershipTimesOneThousand)
+        public
+    {
+
+    }
+}
