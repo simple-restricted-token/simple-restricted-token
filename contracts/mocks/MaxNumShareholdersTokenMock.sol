044d2efc96786c75aa232bdf1b83096c516d7b9e
Ron Gierlach <rongierlach@gmail.com>
2018-08-14 18:08:36 -0700

    reorder inheritance

--- contracts/mocks/MaxNumShareholdersTokenMock.sol
+++ contracts/mocks/MaxNumShareholdersTokenMock.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.24;
 import "./BasicTokenMock.sol";
 import "../examples/shareholder-rules/MaxNumShareholdersToken.sol";
 
-contract MaxNumShareholdersTokenMock is MaxNumShareholdersToken, BasicTokenMock {
+contract MaxNumShareholdersTokenMock is BasicTokenMock, MaxNumShareholdersToken {
     constructor (address initialAccount, uint256 initialBalance, uint256 _maxNumShareholders)
-        MaxNumShareholdersToken(_maxNumShareholders)
         BasicTokenMock(initialAccount, initialBalance)
+        MaxNumShareholdersToken(_maxNumShareholders)
         public
     {
 
