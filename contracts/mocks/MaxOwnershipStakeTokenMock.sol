3359a2a1f15e67c3d9129669cbddd96376c9af3b
Ron Gierlach <rongierlach@gmail.com>
2018-08-17 13:52:51 -0700

    Update MaxOwnershipStakeTokenMock.sol

--- contracts/mocks/MaxOwnershipStakeTokenMock.sol
+++ contracts/mocks/MaxOwnershipStakeTokenMock.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 import "./BasicTokenMock.sol";
 import "../examples/cap-tables/MaxOwnershipStakeToken.sol";
 
-contract MaxOwnershipStakeTokenMock is MaxOwnershipStakeToken, BasicTokenMock {
+contract MaxOwnershipStakeTokenMock is BasicTokenMock, MaxOwnershipStakeToken {
     constructor (address initialAccount, uint256 initialBalance, uint256 _maximumPercentOwnershipTimesOneThousand)
         BasicTokenMock(initialAccount, initialBalance)
         MaxOwnershipStakeToken(_maximumPercentOwnershipTimesOneThousand)
