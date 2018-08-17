d64d46c9b66443017f8de95c080e8a5f8834892e
Ron Gierlach <rongierlach@gmail.com>
2018-08-17 13:53:25 -0700

    Update BasicTokenMock.sol

--- contracts/mocks/BasicTokenMock.sol
+++ contracts/mocks/BasicTokenMock.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 import "zeppelin-solidity/contracts/token/ERC20/BasicToken.sol";
 
-/// @title Mock class using Open Zepplein's BasicToken
+/// @title Mock token contract using Open Zepplein's BasicToken
 /// @dev Adapted from https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/mocks/BasicTokenMock.sol
 contract BasicTokenMock is BasicToken {
     constructor (address initialAccount, uint256 initialBalance) public {
